92bc2a439342765458ff2c7d010e0969c0398ec8
Zentetsu <10742751+Zentetsu@users.noreply.github.com>
2020-07-22 00:04:50 +0200

    Adding comments

--- IRON/Module.py
+++ IRON/Module.py
@@ -5,7 +5,7 @@ Author: Zentetsu
 
 ----
 
-Last Modified: Tue Jul 21 2020
+Last Modified: Wed Jul 22 2020
 Modified By: Zentetsu
 
 ----
@@ -31,6 +31,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ----
 
 HISTORY:
+2020-07-22	Zen	Adding comments and Availability method
 2020-07-17	Zen	fix for addListener and delListener
 2020-07-13	Zen	Draft finished (not tested yet)
 2020-07-08	Zen	Draft (not tested yet)
@@ -43,7 +44,18 @@ import json
 
 
 class Module:
-    def __init__(self, name=None, file=None):
+    """Module class focused on vommunicate data with other modules
+    """
+    def __init__(self, name:str=None, file:str=None):
+        """Class constructor
+
+        Args:
+            name (str, optional): desired name for the module. Defaults to None.
+            file (str, optional): path to load JSON file and construct module following is content. Defaults to None.
+
+        Raises:
+            IRONMultiInputError: raise an error when value and path are both at None or initilalized
+        """
         if name is None and file is None or name is not None and file is not None:
             raise IRONMultiInputError()
 
@@ -54,7 +66,13 @@ class Module:
             self.sender = {}
             self.listener = {}
 
-    def _loadJSON(self, file):
+    def _loadJSON(self, file:str):
+        """Method to load the module structure from a JSON file
+
+        Args:
+            file (str): path of the JSON file
+        """
+        #TODO need to be tested
         json_file = open(file)
         value = json.load(json_file)
         json_file.close()
@@ -65,20 +83,45 @@ class Module:
         self.sender = value["sender"]
         self.listener = value["listener"]
 
-    def dumpJSON(self, file):
+    def dumpJSON(self, file:str):
+        """Method to save the module structure into a JSON file
+
+        Args:
+            file (str): path to the JSON file
+        """
+        #TODO need to be tested
         _dict = {"name": self.name, "sender": self.sender, "listener": self.listener}
         json_file = open(file)
         value = json.dump(json_file)
         json_file.close()
 
-    def _checkIntegrity(self, value: dict):
+    def _checkIntegrity(self, value:dict):
+        """Method to chech the integrity of the module structure extract from the JSON file
+
+        Args:
+            value (dict): dict that's containt values of the module
+
+        Raises:
+            IRONKeyMissing: raise an error when one of the principal key is not into the dict
+            IRONSenderListenerEmpty: raise ann error when there(re not listener and sender into the dict)
+        """
         if not all([n in value.key() for n in ["name", "sender", "listener"]]):
             raise IRONKeyMissing
 
         if not value["sender"] and not value["listener"]:
             raise IRONSenderListenerEmpty
 
-    def _checkNameExistOrNot(self, name, exist=True):
+    def _checkNameExistOrNot(self, name:str, exist=True):
+        """Method that check if a name is already or not used by a shared memory
+
+        Args:
+            name (str): Shared Memory name
+            exist (bool, optional): True -> name mst be defined, False -> name must be undefined. Defaults to True.
+
+        Raises:
+            IRONNameNotExist: raise an error if name doesn't exist
+            IRONNameExist: raise an error if name exist
+        """
         if exist:
             if name not in self.listener.keys() and name not in self.sender.keys():
                 raise IRONNameNotExist(name)
@@ -86,29 +129,63 @@ class Module:
             if name in self.listener.keys() or name in self.sender.keys():
                 raise IRONNameExist(name)
 
-    def addListener(self, name, timeout=1):
+    def addListener(self, name:str, timeout=1):
+        """Method to add a Shared Memory Server
+
+        Args:
+            name (str): Shared Memory Name
+            timeout (int, optional): mutex timeout. Defaults to 1.
+        """
         self._checkNameExistOrNot(name, False)
 
         self.listener[name] = Server(name, timeout)
 
-    def delListener(self, name):
+    def delListener(self, name:str):
+        """Method to remove a Shared Memory Server
+
+        Args:
+            name ([type]): Shared Memory name
+        """
         self._checkNameExistOrNot(name)
 
         self.stopModule(name)
         self.listener.pop(name)
 
-    def addSender(self, name, value=None, path=None, size=10, timeout=1):
+    def addSender(self, name:str, value=None, path:str=None, size=10, timeout=1):
+        """Method to add a Shared Memory Client
+
+        Args:
+            name (str): Shared Memory name
+            value ([type], optional): value to share with the other module. Defaults to None.
+            path (str, optional): path to load JSON file and share the data inside. Defaults to None.
+            size (int, optional):  or str value. Defaults to 10.
+            timeout (int, optional): mutex timeout. Defaults to 1.
+        """
         self._checkNameExistOrNot(name, False)
 
         self.sender[name] = Client(name, value, path, size, timeout)
 
-    def delSender(self, name):
+    def delSender(self, name:str):
+        """Method to remove a Shared Memory Client
+
+        Args:
+            name (str): Shared Memory name
+        """
         self._checkNameExistOrNot(name)
 
         self.stopModule(name)
         self.sender.pop(name)
 
-    def getLSName(self, listener=True, sender=True):
+    def getLSName(self, listener=True, sender=True) -> [list, list]:
+        """Method to return al liste that contains name of sender and listener
+
+        Args:
+            listener (bool, optional): True -> will add listener names to the list. Defaults to True.
+            sender (bool, optional): True -> will ad sender names to the list. Defaults to True.
+
+        Returns:
+            [list, list]: list of sender and listener names
+        """
         _sender = []
         _listener = []
 
@@ -120,7 +197,15 @@ class Module:
 
         return  _sender, _listener
 
-    def getValue(self, name):
+    def getValue(self, name:str):
+        """Method to get value from a sender or a listener
+
+        Args:
+            name (str): name of the sender or listener
+
+        Returns:
+            [type]: return data
+        """
         self._checkNameExistOrNot(name, True)
 
         if name in self.sender.keys():
@@ -128,7 +213,13 @@ class Module:
         else:
             return self.listener[name].getValue()
 
-    def setValue(self, name, value):
+    def setValue(self, name:str, value):
+        """Method to update data
+
+        Args:
+            name (str): name of the sender or listener
+            value ([type]): new value to assign
+        """
         self._checkNameExistOrNot(name, True)
 
         if name in self.sender.keys():
@@ -136,7 +227,16 @@ class Module:
         else:
             self.listener[name].updateValue(value)
 
-    def getLSAvailability(self, listener=None, sender=None):
+    def getLSAvailability(self, listener=True, sender=True) -> [list, list]:
+        """Method to get the availability of each sender and listener
+
+        Args:
+            listener (bool, optional): True -> will add listener availability. Defaults to None.
+            sender (bool, optional): True -> will add sender availability. Defaults to None.
+
+        Returns:
+            [list, list]: list of the sender and listener availability
+        """
         _sender = []
         _listener = []
 
@@ -148,7 +248,12 @@ class Module:
 
         return  _sender, _listener
 
-    def startModule(self, name=None):
+    def startModule(self, name:str=None):
+        """Method to start senders and listeners
+
+        Args:
+            name (str, optional): if setted will launch only this one. Defaults to None.
+        """
         if name is not None:
             self._checkNameExistOrNot(name)
             if name in self.sender.keys():
@@ -162,7 +267,12 @@ class Module:
             for n in self.listener.keys():
                 self.listener[n].connect()
 
-    def stopModule(self, name=None):
+    def stopModule(self, name:str=None):
+        """Method to stop senders and listeners
+
+        Args:
+            name (str, optional): if setted will stop only this one. Defaults to None.
+        """
         if name is not None:
             if name in self.sender.keys():
                 self.sender[name].stop()
@@ -175,7 +285,12 @@ class Module:
             for n in self.listener.keys():
                 self.listener[n].stop()
 
-    def restartModule(self, name=None):
+    def restartModule(self, name:str=None):
+        """Method to restart senders and listeners
+
+        Args:
+            name (str, optional): if setted will restart only this one. Defaults to None.
+        """
         if name is not None:
             self._checkNameExistOrNot(name)
             if name in self.sender.keys():
@@ -191,6 +306,11 @@ class Module:
                 self.listener[n].reconnect()
 
     def __repr__(self):
+        """Redefined method to print value of the Module Class instance
+
+        Returns:
+            str: printable value of Module Class instance
+        """
         s = "Name: " + self.name + "\n"\
             + "\tSender: " + list(self.sender.keys()).__repr__() + "\n"\
             + "\tListener: " + list(self.listener.keys()).__repr__()
