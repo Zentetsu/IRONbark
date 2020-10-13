e11949952b8c51ba3f11b9813eb140e2d188638c
Zentetsu <10742751+Zentetsu@users.noreply.github.com>
2020-10-13 21:19:40 +0200

    Updating test

--- tests/test_module.py
+++ tests/test_module.py
@@ -5,7 +5,7 @@ Author: Zentetsu
 
 ----
 
-Last Modified: Sun Jul 26 2020
+Last Modified: Tue Oct 13 2020
 Modified By: Zentetsu
 
 ----
@@ -31,6 +31,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ----
 
 HISTORY:
+2020-10-13	Zen	Updating test
 2020-07-23	Zen	Adding test for JSON file
 2020-07-08	Zen	Creating file
 '''
@@ -38,220 +39,263 @@ HISTORY:
 
 # from context import Module
 from IRONbark.Module import Module
+import contextlib
 
-def test_error_None():
-    m = Module("test0")
-    print(m)
-    assert m.getLSName() == ([], [])
-
-def test_add_sender():
-    m = Module("test1")
-    m.addSender("name", value=10)
-    print(m)
-    assert m.getValue("name") == 10
-    m.stopModule()
-
-def test_add_listener():
-    m = Module("test2")
+def test_creation():
+    print("Create Module instance:", end=" ")
     try:
-        m.addListener("name")
-        print(m)
-        m.stopModule()
-        assert False
+        m = Module("test0")
+        assert m.getLSName() == ([], [])
+        print("SUCCESSED")
     except:
-        assert True
+        print("FAILED")
+        assert False
 
-def test_add_listener2():
-    m = Module("test3a")
-    m.addSender("name", value=10)
-    print(m)
-    m2 = Module("test3b")
-    m2.addListener("name")
-    print(m2)
-    assert m2.getValue("name") == 10
-    m.stopModule()
-    m2.stopModule()
+def test_addSender():
+    print("Add Sender to Module:", end=" ")
+    try:
+        with contextlib.redirect_stdout(None):
+            m = Module("test1")
+            m.addSender("name", value=10)
+            assert m.getValue("name") == 10
+            m.stopModule()
+        print("SUCCESSED")
+    except:
+        print("FAILED")
+        assert False
+
+def test_addListener():
+    print("Add Listener to Module without Sender:", end=" ")
+    try:
+        with contextlib.redirect_stdout(None):
+            m = Module("test2")
+            m.addListener("name")
+            assert m.getValue("name") == None
+            m.stopModule()
+        print("SUCCESSED")
+    except:
+        print("FAILED")
+        assert False
+
+def test_addListener2():
+    print("Add Listener to Module with Sender:", end=" ")
+    try:
+        with contextlib.redirect_stdout(None):
+            m = Module("test3")
+            m.addSender("name", value=10)
+            m2 = Module("test3b")
+            m2.addListener("name")
+            assert m2.getValue("name") == 10
+            m.stopModule()
+            m2.stopModule()
+        print("SUCCESSED")
+    except:
+        print("FAILED")
+        assert False
 
 def test_restart():
-    m = Module("test4")
-    m.addSender("name", value=10)
-    print(m)
+    print("Restarting Sender from Module:", end=" ")
     try:
-        m.restartModule("name")
-        m.stopModule()
+        with contextlib.redirect_stdout(None):
+            m = Module("test4")
+            m.addSender("name", value=10)
+            m.restartModule("name")
+            m.stopModule()
         assert True
+        print("SUCCESSED")
     except:
+        print("FAILED")
         assert False
 
-def test_stop_start():
-    m = Module("test5")
-    m.addSender("name", value=10)
-    print(m)
+def test_stopStart():
+    print("Restarting Sender from Module bis:", end=" ")
     try:
-        m.stopModule("name")
-        m.startModule("name")
-        m.stopModule()
+        with contextlib.redirect_stdout(None):
+            m = Module("test5")
+            m.addSender("name", value=10)
+            m.stopModule("name")
+            m.startModule("name")
+            m.stopModule()
         assert True
+        print("SUCCESSED")
     except:
-        assert False
-
-def test_stop_start2():
-    m = Module("test6")
-    m.addSender("name", value=10)
-    print(m)
-    try:
-        m.stopModule()
-        m.startModule()
-        m.stopModule()
-        assert True
-    except:
+        print("FAILED")
         assert False
 
 def test_setValue():
-    m = Module("test7")
-    m.addSender("name", value=10)
-    print(m)
-    m.setValue("name", 20)
-    print(m)
-    assert m.getValue("name") == 20
-    m.stopModule()
+    print("Sender edit value:", end=" ")
+    try:
+        with contextlib.redirect_stdout(None):
+            m = Module("test7")
+            m.addSender("name", value=10)
+            m.setValue("name", 20)
+            assert m.getValue("name") == 20
+            m.stopModule()
+        print("SUCCESSED")
+    except:
+        print("FAILED")
+        assert False
 
 def test_setValue2():
-    m = Module("test8a")
-    m.addSender("name", value=10)
-    print(m)
-    m2 = Module("test8b")
-    m2.addListener("name")
-    print(m2)
-    m.setValue("name", 20)
-    print(m)
-    print(m2)
-    assert m2.getValue("name") == 20
-    m.stopModule()
-    m2.stopModule()
+    print("Sender edit value bis:", end=" ")
+    try:
+        with contextlib.redirect_stdout(None):
+            m = Module("test8a")
+            m.addSender("name", value=10)
+            m2 = Module("test8b")
+            m2.addListener("name")
+            m.setValue("name", 20)
+            assert m2.getValue("name") == 20
+            m.stopModule()
+            m2.stopModule()
+        print("SUCCESSED")
+    except:
+        print("FAILED")
+        assert False
 
 def test_setValue3():
-    m = Module("test9a")
-    m.addSender("name", value=10)
-    print(m)
-    m2 = Module("test9b")
-    m2.addListener("name")
-    print(m2)
-    m2.setValue("name", 20)
-    print(m)
-    print(m2)
-    assert m.getValue("name") == 20
-    m.stopModule()
-    m2.stopModule()
+    print("Listener edit value:", end=" ")
+    try:
+        with contextlib.redirect_stdout(None):
+            m = Module("test9a")
+            m.addSender("name", value=10)
+            m2 = Module("test9b")
+            m2.addListener("name")
+            m2.setValue("name", 20)
+            assert m.getValue("name") == 20
+            m.stopModule()
+            m2.stopModule()
+        print("SUCCESSED")
+    except:
+        print("FAILED")
+        assert False
 
-def test_del_sender():
-    m = Module("test10")
-    m.addSender("name", value=10)
-    print(m)
-    _s, _ = m.getLSName()
-    assert len(_s) == 1
-    m.delSender("name")
-    print(m)
-    _s, _ = m.getLSName()
-    assert len(_s) == 0
-    m.stopModule()
+def test_delSender():
+    print("Removing Sender:", end=" ")
+    try:
+        with contextlib.redirect_stdout(None):
+            m = Module("test10")
+            m.addSender("name", value=10)
+            _s, _ = m.getLSName()
+            assert len(_s) == 1
+            m.delSender("name")
+            _s, _ = m.getLSName()
+            assert len(_s) == 0
+            m.stopModule()
+        print("SUCCESSED")
+    except:
+        print("FAILED")
+        assert False
 
-def test_del_listener():
-    m = Module("test11a")
-    m2 = Module("test11b")
-    m.addSender("name", value=10)
-    m2.addListener("name")
-    print(m)
-    print(m2)
-    _, _l = m2.getLSName()
-    assert len(_l) == 1
-    m2.delListener("name")
-    print(m2)
-    _, _l = m2.getLSName()
-    assert len(_l) == 0
-    m.stopModule()
-    m2.stopModule()
+def test_delListener():
+    print("Removing Listener:", end=" ")
+    try:
+        with contextlib.redirect_stdout(None):
+            m = Module("test11a")
+            m2 = Module("test11b")
+            m.addSender("name", value=10)
+            m2.addListener("name")
+            _, _l = m2.getLSName()
+            assert len(_l) == 1
+            m2.delListener("name")
+            _, _l = m2.getLSName()
+            assert len(_l) == 0
+            m.stopModule()
+            m2.stopModule()
+        print("SUCCESSED")
+    except:
+        print("FAILED")
+        assert False
 
 def test_():
-    m = Module("test12a")
-    m2 = Module("test12b")
-    m.addSender("name", value=10)
-    m2.addListener("name")
-    print(m)
-    print(m2)
-    m.stopModule("name")
-    print(m)
-    print(m2)
-    m.stopModule()
-    m2.stopModule()
+    print("Stopping Modules:", end=" ")
+    try:
+        with contextlib.redirect_stdout(None):
+            m = Module("test12a")
+            m2 = Module("test12b")
+            m.addSender("name", value=10)
+            m2.addListener("name")
+            m.stopModule("name")
+            m.stopModule()
+            m2.stopModule()
+        assert True
+        print("SUCCESSED")
+    except:
+        print("FAILED")
+        assert False
 
 def test_availability():
-    m = Module("test13")
-    m.addSender("name", value=10)
-    print(m)
-    assert m.getLSAvailability(sender=True)[0][0] == (True, False)
-    m.stopModule()
+    print("Testing availability:", end=" ")
+    try:
+        with contextlib.redirect_stdout(None):
+            m = Module("test13")
+            m.addSender("name", value=10)
+            assert m.getLSAvailability(sender=True)[0][0] == (True, False)
+            m.stopModule()
+        print("SUCCESSED")
+    except:
+        print("FAILED")
+        assert False
 
-def test_availability():
-    m = Module("test14a")
-    m2 = Module("test14b")
-    m.addSender("name", value=10)
-    m2.addListener("name")
-    print(m)
-    print(m.getLSAvailability(sender=True))
-    assert m.getLSAvailability(sender=True)[0][0] == (True, True)
-    print(m2.getLSAvailability(listener=True))
-    assert m2.getLSAvailability(listener=True)[1][0] == (True, True)
-    m.stopModule()
-    m2.stopModule()
+def test_availability2():
+    print("Testing availability bis:", end=" ")
+    try:
+        with contextlib.redirect_stdout(None):
+            m = Module("test14a")
+            m2 = Module("test14b")
+            m.addSender("name", value=10)
+            m2.addListener("name")
+            assert m.getLSAvailability(sender=True)[0][0] == (True, True)
+            assert m2.getLSAvailability(listener=True)[1][0] == (True, True)
+            m.stopModule()
+            m2.stopModule()
+        print("SUCCESSED")
+    except:
+        print("FAILED")
+        assert False
 
 def test_JSON():
-    m = Module(file="tests/test.json")
-    print(m)
-    print(m.getValue("sender1"))
-    assert m.getValue("sender1") == {'test': [10, 30, True], 'test2': ['str', 1.2]}
-    m.stopModule()
+    print("Creating Module from JSON file:", end=" ")
+    try:
+        with contextlib.redirect_stdout(None):
+            m = Module(file="tests/test.json")
+            assert m.getValue("sender1") == {'test': [10, 30, True], 'test2': ['str', 1.2]}
+            m.stopModule()
+        print("SUCCESSED")
+    except:
+        print("FAILED")
+        assert False
 
 def test_JSON2():
-    m = Module(file="tests/test.json")
-    m2 = Module(file="tests/test2.json")
-    print(m)
-    print(m2)
-    print(m2.getValue("sender1"))
-    assert m2.getValue("sender1") == {'test': [10, 30, True], 'test2': ['str', 1.2]}
-    m.stopModule()
-    m2.stopModule()
+    print("Creating Module from JSON file bis:", end=" ")
+    try:
+        with contextlib.redirect_stdout(None):
+            m = Module(file="tests/test.json")
+            m2 = Module(file="tests/test2.json")
+            assert m2.getValue("sender1") == {'test': [10, 30, True], 'test2': ['str', 1.2]}
+            m.stopModule()
+            m2.stopModule()
+        print("SUCCESSED")
+    except:
+        print("FAILED")
+        assert False
 
 
-test_error_None()
 print("-"*10)
-test_add_sender()
-print("-"*10)
-test_add_listener()
-print("-"*10)
-test_add_listener2()
-print("-"*10)
+test_creation()
+test_addSender()
+test_addListener()
+test_addListener2()
 test_restart()
-print("-"*10)
-test_stop_start()
-print("-"*10)
-test_stop_start2()
-print("-"*10)
+test_stopStart()
 test_setValue()
-print("-"*10)
 test_setValue2()
-print("-"*10)
 test_setValue3()
-print("-"*10)
-test_del_sender()
-print("-"*10)
-test_del_listener()
-print("-"*10)
+test_delSender()
+test_delListener()
 test_()
-print("-"*10)
 test_availability()
-print("-"*10)
+test_availability2()
 test_JSON()
-print("-"*10)
 test_JSON2()
 print("-"*10)
